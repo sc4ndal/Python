7ad1a5fd0dddf36e979687bbe5b8fb8f727c6431
HaruAki <100898033+sc4ndal@users.noreply.github.com>
2022-03-21 18:01:02 +0900

    Add files via upload

--- tur01.py
+++ tur01.py
@@ -1,34 +1,34 @@
 import turtle as t
 
-t.setup(960,560)
+t.setup(960,560)   #실행 윈도우창 크기 설정
 
 t.shape('turtle')
-t.speed(0)
-t.pensize(20)
+t.speed(0)       #거북이 속도 
+t.pensize(20)    
 
 #기본으로 검정색으로 그림
+t.circle(100)    #반지름이 100인 원 그리기
+
+t.up()            #펜 올라가서 선 안 그려짐
+t.forward(240)     #우측으로 240픽셀이동
+t.down()          #펜 내려와서 선 그림
+t.pencolor('red')   #펜 색상을 레드로 함
 t.circle(100)
 
 t.up()
-t.forward(240)
+t.backward(480)     #좌측으로 480픽셀 이동
 t.down()
-t.pencolor('red')
+t.pencolor('#1E90FF')    #색상표 번호로 색상 지정
 t.circle(100)
 
 t.up()
-t.backward(480)
-t.down()
-t.pencolor('#1E90FF')
-t.circle(100)
-
-t.up()
-t.sety(-120)
+t.sety(-120)       #거북이 밑으로 120픽셀 이동
 t.down()
 
 t.up()
 t.forward(120)
 t.down()
-t.pencolor('#FFD700')
+t.pencolor('#FFD700')   
 t.circle(100)
 
 t.up()
@@ -37,7 +37,7 @@ t.down()
 t.pencolor('green')
 t.circle(100)
 
-t.hideturtle()
+t.hideturtle()      #거북이 숨김
 
 
 t.done()
